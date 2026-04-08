927be9e68f63916e06c0a304f5bcc8d7b319cefa
E.Z. Hart <hartez@gmail.com>
2026-04-08 09:32:58 -0600

    Pull in unit tests

---
+++ EZtodo.Tests/TaskListTests.cs
@@ -0,0 +1,280 @@
+﻿namespace EZtodo.Tests
+{
+    public class TaskListTests
+    {
+        private const string _testDataPath = "testtasks.txt";
+
+        private string CreateTempTasksFile()
+        {
+            string tempTaskFile = Path.GetRandomFileName();
+            File.Copy(_testDataPath, tempTaskFile, true);
+            return tempTaskFile;
+        }
+
+        [Fact]
+        public void AggregateContexts()
+        {
+            string tempTaskFile = CreateTempTasksFile();
+            var taskList = new TaskList(tempTaskFile);
+            IOrderedEnumerable<string> contexts = taskList.SelectMany(numberedTask => numberedTask.Task.Contexts,
+                                                                (task, context) => context).Distinct().OrderBy(context => context);
+
+            Assert.Equal(3, contexts.Count());
+        }
+
+        [Fact]
+        public void Add_Multiple()
+        {
+            var taskList = new TaskList(_testDataPath);
+            int c = taskList.Count;
+
+            taskList.Add("Add_Multiple task one");
+            taskList.Add("Add_Multiple task two");
+
+            Assert.Equal(c + 2, taskList.Count);
+        }
+
+        [Fact]
+        public void Add_ToFile()
+        {
+            // Create a copy of test data so we can leave the original alone
+            string tempTaskFile = CreateTempTasksFile();
+
+            List<string> fileContents = [.. File.ReadAllLines(tempTaskFile)];
+            fileContents.Add("(B) Add_ToFile +test @task");
+
+            var tl = new TaskList(tempTaskFile);
+            tl.Add(fileContents.Last());
+            tl.Save(tempTaskFile);
+
+            string[] newFileContents = File.ReadAllLines(tempTaskFile);
+
+            Assert.Equivalent(fileContents, newFileContents);
+
+            // Clean up
+            File.Delete(tempTaskFile);
+        }
+
+        [Fact]
+        public void BlankLinesAreEmptyTasks()
+        {
+            // Create a copy of test data so we can leave the original alone
+            string tempTaskFile = CreateTempTasksFile();
+
+            var tl = new TaskList(tempTaskFile);
+            var originalCount = tl.Count;
+
+            File.AppendAllText(tempTaskFile, Environment.NewLine + Environment.NewLine + "The above line was blank" + Environment.NewLine);
+
+            var st = File.Open(tempTaskFile, FileMode.Open);
+
+            st.Close();
+
+            var tl2 = new TaskList(tempTaskFile);
+
+            Assert.Equal(originalCount + 2, tl2.Count); // "Added two lines, one of which was blank (empty)"
+        }
+
+        [Fact]
+        public void Add_To_Empty_File()
+        {
+            // v0.3 and earlier contained a bug where a blank task was added
+            string tempTaskFile = CreateTempTasksFile();
+            File.WriteAllLines(tempTaskFile, new string[] { }); // empties the file
+
+            var tl = new TaskList(tempTaskFile)
+            {
+                "A task"
+            };
+
+            Assert.Single(tl);
+
+            // Clean up
+            File.Delete(tempTaskFile);
+        }
+
+        [Fact]
+        public void Construct()
+        {
+            _ = new TaskList(_testDataPath);
+        }
+
+        [Fact]
+        public void Delete_InFile()
+        {
+            string tempTasksFile = CreateTempTasksFile();
+            try
+            {
+                string[] fileLines = File.ReadAllLines(tempTasksFile);
+                List<string> fileContents = [.. fileLines];
+                var task = Task.Parse(fileContents.Last());
+                fileContents.Remove(fileContents.Last());
+
+                var tl = new TaskList(tempTasksFile);
+                tl.RemoveTask(tl.Last().Number);
+                tl.Save(tempTasksFile);
+
+                string[] newFileContents = File.ReadAllLines(tempTasksFile);
+                Assert.Equivalent(fileContents, newFileContents);
+            }
+            finally
+            {
+                File.Delete(tempTasksFile);
+            }
+        }
+
+        [Fact]
+        public void Load_From_File()
+        {
+            var tl = new TaskList(_testDataPath);
+            Assert.Equal(8, tl.Count);
+            Assert.Equal("A task", tl.GetTask(1).Body);
+        }
+
+        [Fact]
+        public void Search()
+        {
+            string tempTasksFile = CreateTempTasksFile();
+            var tl = new TaskList(tempTasksFile);
+
+            // There should be two tasks which contain the term 'foo'
+            var fooTaskList = tl.Search("foo");
+            Assert.NotNull(fooTaskList);
+            Assert.Equal(2, fooTaskList.Count());
+
+            // Search should be case insenstive
+            var caseInsensitiveTaskList = tl.Search("Foo");
+            Assert.NotNull(caseInsensitiveTaskList);
+            Assert.Equal(2, caseInsensitiveTaskList.Count());
+
+            // '-' in front of the term should find all tasks without the term
+            var notFooTaskList = tl.Search("-foo");
+            
+            // So searching the list generated by the negative search for the term
+            // should give us an empty list
+            Assert.DoesNotContain(notFooTaskList, nt => nt.Task.Body.Contains("foo"));
+        }
+
+        [Fact]
+        public void ToggleComplete_Off_InCollection()
+        {
+            // Not complete - doesn't include completed date
+            var taskList = new TaskList(_testDataPath)
+            {
+                "X (B) ToggleComplete_Off_InCollection +test @task"
+            };
+
+            var itemNumber = taskList.Count;
+
+            taskList.ToggleCompleted(itemNumber);
+
+            Assert.True(taskList.Last().Completed);
+
+            taskList.Add("x 2011-02-25 ToggleComplete_Off_InCollection +test @task");
+
+            itemNumber = taskList.Count;
+
+            taskList.ToggleCompleted(itemNumber);
+
+            Assert.False(taskList.Last().Completed);
+        }
+
+        [Fact]
+        public void ToggleComplete_On_InCollection()
+        {
+            var taskList = new TaskList(_testDataPath)
+            {
+                "(B ToggleComplete_On_InCollection +test @task"
+            };
+
+            taskList.ToggleCompleted(taskList.Count);
+
+            Assert.True(taskList.Last().Completed);
+        }
+
+        [Fact]
+        public void UpdateTaskPriority()
+        {
+            var text = @"this is the first task
+this is the second task
+";
+
+            var tl = new TaskList
+            {
+                text
+            };
+
+            Assert.Null(tl.GetTask(1).Priority);
+
+            tl.SetItemPriority(1, 'a');
+            Assert.Equal('A', tl.GetTask(1).Priority);
+        }
+
+        [Fact]
+        public void MarkCompleteSetsCompletedDate()
+        {
+            var taskList = new TaskList(_testDataPath);
+            taskList.MarkCompleted(8);
+            var task = taskList.GetTask(8);
+
+            Assert.True(task.Completed);
+            Assert.NotNull(task.CompletedDate);
+        }
+
+        [Fact]
+        public void AddDoNotEnsureCreatedDate()
+        {
+            var hasCreatedDate = "2025-04-03 Has a created date";
+            var noCreatedDate = "Has no created date";
+         
+            var taskList = new TaskList();
+
+            taskList.Create(hasCreatedDate, ensureCreatedDate: false);
+            taskList.Create(noCreatedDate, ensureCreatedDate: false);
+            
+            Assert.NotNull(taskList.GetTask(1).CreatedDate);
+            Assert.Null(taskList.GetTask(2).CreatedDate);
+        }
+        
+        [Fact]
+        public void AddEnsureCreatedDate()
+        {
+            var hasCreatedDate = "2025-04-03 Has a created date";
+            var noCreatedDate = "Has no created date";
+         
+            var taskList = new TaskList();
+            
+            taskList.Create(hasCreatedDate, ensureCreatedDate: true);
+            taskList.Create(noCreatedDate, ensureCreatedDate: true);
+            
+            Assert.NotNull(taskList.GetTask(1).CreatedDate);
+            Assert.NotNull(taskList.GetTask(2).CreatedDate);
+
+            // Make sure that we didn't get an extra date dropped into the task that already had one
+            Assert.Equal(hasCreatedDate, taskList.GetTask(1).ToString());
+        }
+
+        [Fact]
+        public void NumbersInStringOutput()
+        { 
+            var taskList = new TaskList()
+            {
+                "this is task 1",
+                "this is task 2",
+                "this is task 3",
+                "this is task 4",
+                "this is task 5",
+                "this is task 6",
+                "this is task 7",
+                "this is task 8",
+                "this is task 9",
+                "this is task 10",
+            };
+
+            var output = taskList.ToString();
+            Assert.Contains("06", output);
+
+            Console.Write(output);
+        }
+    }
+}
---
+++ EZtodo.Tests/TaskTests.cs
@@ -0,0 +1,237 @@
+﻿namespace EZtodo.Tests;
+
+public class TaskTests
+{
+    [Fact]
+    public void Priority_Case()
+    {
+        // Should have a priority of A
+        var task1 = Task.Parse("(A) This is a test task");
+
+        // Doesn't fit the priority rule - should have no priority
+        var task2 = Task.Parse("(a) This is a test task");
+
+        Assert.Equal('A', task1.Priority);
+
+        Assert.Null(task2.Priority);
+        Assert.Equal("(a) This is a test task", task2.Body);
+    }
+
+    [Fact]
+    public void CompletedDate()
+    {
+        var task1 = Task.Parse("x 2011-03-01 2010-12-31 This task should be completed");
+
+        Assert.NotNull(task1.CompletedDate);
+        Assert.Equal(new DateTime(2011, 3, 1), task1.CompletedDate);
+
+        var task2 = Task.Parse("2010-12-31 This task should not be completed");
+
+        Assert.Null(task2.CompletedDate);
+
+        var task3 = Task.Parse(task1.ToString());
+        Assert.NotNull(task3.CompletedDate);
+        Assert.Equal(new DateTime(2011, 3, 1), task3.CompletedDate);
+    }
+
+    #region Create
+    [Fact]
+    public void Constructor_Ignores_Trailing_Whitespace()
+    {
+        var task = Task.Parse("(A) This is a test task @work +test  ");
+
+        var expectedTask = Task.Parse("(A) This is a test task @work +test");
+        Assert.Equal(expectedTask, task);
+    }
+
+    [Fact]
+    public void Create_Null_Priority()
+    {
+        var task = Task.Parse("This is a test task @work +test ");
+
+        Assert.Null(task.Priority);
+    }
+
+    [Fact]
+    public void Priority_Must_Be_First()
+    {
+        var task = Task.Parse("Oh (A) This is a test task @work +test ");
+        Assert.Null(task.Priority);
+    }
+
+
+
+    [Fact]
+    public void Completed_Task()
+    {
+        var task = Task.Parse("x 2026-04-02 This task is completed");
+        Assert.True(task.Completed);
+    }
+
+    [Fact]
+    public void Completed_Must_Begin_With_x()
+    {
+        var task = Task.Parse("X 2026-04-02 This task is not completed");
+        Assert.False(task.Completed);
+    }
+
+    [Fact]
+    public void Completed_Must_Include_Completed_Date()
+    {
+        var task = Task.Parse("x This task is not completed");
+        Assert.False(task.Completed);
+    }
+
+    [Fact]
+    public void Project_And_Context()
+    {
+        var task = Task.Parse("This is a test task @work +test ");
+        Assert.Contains("+test", task.Projects);
+        Assert.Contains("@work", task.Contexts);
+
+        Assert.DoesNotContain("+test", task.Contexts);
+        Assert.DoesNotContain("@work", task.Projects);
+    }
+
+
+    [Fact]
+    public void Multiple_Projects()
+    {
+        var task = Task.Parse("(A) This is a test task @work +test +test2");
+
+        Assert.Contains("+test", task.Projects);
+        Assert.Contains("+test2", task.Projects);
+    }
+
+    [Fact]
+    public void Multiple_Contexts()
+    {
+        var task = Task.Parse("(A) This is a test task @work @home +test");
+
+        Assert.Contains("@work", task.Contexts);
+        Assert.Contains("@home", task.Contexts);
+    }
+
+    [Fact]
+    public void DueDate()
+    {
+        var task = Task.Parse("(A) This is a test task @work @home +test due:2011-05-08");
+        Assert.Equal("2011-05-08", task.DueDate);
+    }
+
+    #endregion
+
+    [Fact]
+    public void BodyOnly()
+    {
+        var task = Task.Parse("test");
+
+        Assert.NotEmpty(task.Body);
+        Assert.Equal("test", task.Body);
+    }
+
+    #region ToString
+
+    [Fact]
+    public void ToString_Matches_Input_String()
+    {
+        var expected = "(A) @work +test This is a test task";
+
+        var task = Task.Parse(expected);
+        Assert.Equal(expected, task.ToString());
+    }
+
+    [Fact]
+    public void ToString_Matches_Input_String_With_DueDate()
+    {
+        var expected = "(A) @work +test This is a test task due:2011-07-08";
+
+        var task = Task.Parse(expected);
+        Assert.Equal(expected, task.ToString());
+    }
+
+    [Fact]
+    public void ToString_Matches_Input_String_With_Contexts_And_Projects()
+    {
+        var expected = "(A) @work +test This is a @context test +project task";
+
+        var task = Task.Parse(expected);
+        Assert.Equal(expected, task.ToString());
+    }
+
+    [Fact]
+    public void Empty_Task_Outputs_Blank_Line()
+    {
+        Assert.Equal("", Task.Empty.ToString());
+    }
+
+    #endregion
+
+    [Fact]
+    public void Equality()
+    {
+        var rawString = "This is a task @online @home +project +anotherproject";
+        Task a = Task.Parse(rawString);
+        Task b = Task.Parse(rawString);
+        Task c = Task.Parse("This is different task @home +anotherproject");
+
+        Assert.Equal(a, b);
+        Assert.NotEqual(b, c);
+        Assert.NotEqual(a, c);
+    }
+
+    [Fact]
+    public void ShouldAllowProjectWithHyphen()
+    {
+        // "A project or context contains any non-whitespace character and must end in an alphanumeric or ‘_’. "
+
+        var task = Task.Parse("This task contains a project with a hypen +hyphen-project @home");
+
+        Assert.Contains("+hyphen-project", task.Projects);
+    }
+
+    [Fact]
+    public void ShouldAllowContextWithHyphen()
+    {
+        // "A project or context contains any non-whitespace character and must end in an alphanumeric or ‘_’. "
+
+        var task = Task.Parse("This task contains a project with a hypen @hyphen-context @home");
+
+        Assert.Contains("@hyphen-context", task.Contexts);
+    }
+
+    [Fact]
+    public void ProjectMustEndWithAlphanumericOrUnderscore()
+    {
+        // "A project or context contains any non-whitespace character and must end in an alphanumeric or ‘_’. "
+
+        var task = Task.Parse("This task has only one project with a hyphen in it +hyphen-project +nohyphen-");
+
+        Assert.Contains("+hyphen-project", task.Projects);
+        Assert.DoesNotContain("+nohyphen-", task.Projects);
+        Assert.Contains("+nohyphen", task.Projects);
+    }
+
+    [Fact]
+    public void ContextMustEndWithAlphanumericOrUnderscore()
+    {
+        // "A project or context contains any non-whitespace character and must end in an alphanumeric or ‘_’. "
+
+        var task = Task.Parse("This task has only one valid context @hyphen-context @nohyphen-");
+
+        Assert.Contains("@hyphen-context", task.Contexts);
+        Assert.DoesNotContain("@nohyphen-", task.Contexts);
+        Assert.Contains("@nohyphen", task.Contexts);
+    }
+
+    [Fact]
+    public void MetaDataMustNotHaveWhitespaceAfterColon()
+    {
+        var task =
+            Task.Parse(
+                "due:2014-07-02 Get back to work on project x Ansel 453.456.8967  Jim 432.453.9134  Bob's cell: 812.477.7272");
+
+        Assert.True(task.DueDate == "2014-07-02");
+        Assert.False(task.Metadata.ContainsKey("cell"), "metadata cannot have whitespace");
+    }
+}
