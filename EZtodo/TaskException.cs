169fd4e9e5c2e3c58858c2f846298780812b4993
E.Z. Hart <hartez@gmail.com>
2026-04-12 14:41:48 -0600

    Fix formatting

--- EZtodo/TaskException.cs
+++ EZtodo/TaskException.cs
@@ -3,16 +3,16 @@ using System;
 namespace EZtodo
 {
     public class TaskException : Exception
- {
+    {
 
-     public TaskException(string message)
-         : base(message)
-     {
+        public TaskException(string message)
+            : base(message)
+        {
 
-     }
-     public TaskException(string message, Exception ex)
-         : base(message, ex)
-     {
-     }
- }
+        }
+        public TaskException(string message, Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
 }
