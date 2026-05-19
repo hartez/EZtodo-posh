636b15c711518421cb852c435618a84f48893f02
E.Z. Hart <hartez@gmail.com>
2026-05-19 13:35:40 -0600

    Fix typo in test

--- EZtodo-posh.Tests/Get-Task.Tests.ps1
+++ EZtodo-posh.Tests/Get-Task.Tests.ps1
@@ -9,13 +9,13 @@ Describe 'Get-Task' {
         $allTasks.Count | Should -Be 8
     }
 
-    It 'Can search for only tasks which match the searcm term' {
+    It 'Can search for only tasks which match the search term' {
         $tasks = Get-Task ./EZtodo-posh.Tests/todo_example.txt "chapter"
         $tasks.Count | Should -Be 1
         $tasks[0].Task.Body | Should -Be "Outline chapter 5 +Novel @Computer"
     }
 
-    It 'Can search for tasks which do not match the searcm term' {
+    It 'Can search for tasks which do not match the search term' {
         $tasks = Get-Task ./EZtodo-posh.Tests/todo_example.txt "-chapter"
         $tasks.Count | Should -Be 7
         $tasks[0].Task.Body | Should -Be "Call Mom @Phone +Family"
