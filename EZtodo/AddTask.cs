f3c06278aae140a6c44cfda7e9a8fe79afe4ce8c
E.Z. Hart <hartez@gmail.com>
2026-05-20 09:41:33 -0600

    Fix relative path bug in Add-Task

--- EZtodo/AddTask.cs
+++ EZtodo/AddTask.cs
@@ -3,11 +3,8 @@ using System.Management.Automation;
 namespace EZtodo
 {
     [Cmdlet(VerbsCommon.Add, "Task")]
-    public class AddTask : PSCmdlet
+    public class AddTask : ReadTaskListCmdLet
     {
-        [Parameter(Mandatory = true, Position = 0)]
-        public string SourcePath { get; set; }
-
         [Parameter(Mandatory = true, Position = 1, ValueFromPipeline = true)]
         public string Task { get; set; }
 
@@ -17,16 +14,9 @@ namespace EZtodo
             get; set;
         }
 
-        private TaskList _list;
-
-        protected override void BeginProcessing()
-        {
-            _list = new TaskList(SourcePath);
-        }
-    
         protected override void ProcessRecord()
         {
-            var newTask = _list.Create(Task, EnsureCreatedDate);
+            var newTask = TaskList.Create(Task, EnsureCreatedDate);
 
             WriteVerbose(newTask.Task.ToString());
             WriteVerbose($"{newTask.Number} added.");
@@ -34,8 +24,8 @@ namespace EZtodo
 
         protected override void EndProcessing()
         {
-            _list.Save(SourcePath);
+            TaskList.Save(SourcePath);
         }
     }
-    
+
 }
